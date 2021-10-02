a81e470e38deb32ea8fac1e847f2402d96013a9e
ElCholoGamer <josedanielgrayson@gmail.com>
2021-10-02 16:34:43 -0500

    add minecraft bow

---
+++ Items/Weapons/MinecraftBow.cs
@@ -0,0 +1,56 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using GamerClass.Projectiles.MinecraftBow;
+using Microsoft.Xna.Framework;
+
+namespace GamerClass.Items.Weapons
+{
+    public class MinecraftBow : GamerWeapon
+    {
+        public override int RamUsage => 0;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Pixelated Bow");
+            Tooltip.SetDefault("Uses arrows as ammo\n'Enchants not applicable'");
+        }
+
+        public override void SafeSetDefaults()
+        {
+            item.width = item.height = 15;
+            item.noMelee = true;
+            item.damage = 2;
+            item.channel = true;
+            item.useAmmo = AmmoID.Arrow;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.shoot = 10;
+            item.shootSpeed = 14f;
+            item.value = Item.sellPrice(copper: 50);
+            item.useTime = item.useAnimation = 10;
+            item.autoReuse = true;
+            item.noUseGraphic = true;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Projectile.NewProjectile(position, new Vector2(speedX, speedY), ModContent.ProjectileType<HeldBow>(), item.damage, knockBack, player.whoAmI, type);
+
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+
+            recipe.AddIngredient(ItemID.WhiteString);
+            recipe.AddIngredient(ItemID.Wood, 10);
+            recipe.AddIngredient(ItemID.StoneBlock, 2);
+
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/MinecraftBow/HeldBow.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GamerClass.Projectiles.MinecraftBow
+{
+    public class HeldBow : ModProjectile
+    {
+        private readonly float DistanceOffset = 14f;
+        private readonly float MaxCharge = 40f;
+
+        public int ProjectileType => (int)projectile.ai[0];
+        public float charge = 0f;
+        public bool charging = true;
+        public float shakeTimer = 0f;
+        public float shakeDirection = -1f;
+
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[projectile.type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = projectile.height = 12;
+            projectile.ignoreWater = true;
+            projectile.tileCollide = false;
+            projectile.penetrate = -1;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[projectile.owner];
+
+            projectile.position = player.Center + projectile.velocity * DistanceOffset;
+
+            UpdatePlayer(player);
+            ChargeBow(player);
+            UpdateShake();
+        }
+
+        private void ChargeBow(Player player)
+        {
+            if (!player.channel)
+            {
+                if (charging)
+                {
+                    ReleaseArrow();
+                    charging = false;
+                }
+            } else
+            {
+                if (charge < MaxCharge) charge++;
+                projectile.timeLeft = 15;
+            }
+
+        }
+
+        private void UpdatePlayer(Player player)
+        {
+            if (charging && Main.myPlayer == projectile.owner)
+            {
+                Vector2 toCursor = Vector2.Normalize(Main.MouseWorld - player.Center);
+                projectile.velocity = toCursor;
+                projectile.direction = Main.MouseWorld.X > player.position.X ? 1 : -1;
+
+                projectile.netUpdate = true;
+            }
+
+            player.ChangeDir(projectile.direction);
+            player.heldProj = projectile.whoAmI;
+            player.itemTime = 2;
+            player.itemAnimation = 2;
+
+            player.itemRotation = (float)Math.Atan2(projectile.velocity.Y * projectile.direction, projectile.velocity.X * projectile.direction);
+        }
+
+        private void UpdateShake()
+        {
+            if (!charging)
+            {
+                shakeDirection = 0f;
+            } else
+            {
+                float timerTarget = 25f - (charge / MaxCharge * 20f);
+                if (++shakeTimer > timerTarget)
+                {
+                    shakeTimer -= timerTarget;
+                    shakeDirection *= -1f;
+                }
+            }
+        }
+
+        private void ReleaseArrow()
+        {
+            if (Main.myPlayer == projectile.owner)
+            {
+                float chargeRadius = charge / MaxCharge;
+                int damage = projectile.damage + (int)(Math.Pow(1.115f, charge));
+                Vector2 position = Main.player[projectile.owner].Center + projectile.velocity * 8f;
+                Vector2 velocity = projectile.velocity * (2f + (chargeRadius * 25f));
+
+                Projectile.NewProjectile(position, velocity, ProjectileType, damage, projectile.knockBack, projectile.owner);
+
+                Main.PlaySound(SoundID.Item5);
+            }
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D texture = Main.projectileTexture[projectile.type];
+            int frameCount = Main.projFrames[projectile.type];
+
+            float chargeRadius = charge / MaxCharge;
+            Vector2 position = Main.player[projectile.owner].Center + projectile.velocity * 5f;
+            position += projectile.velocity.RotatedBy(MathHelper.PiOver2) * shakeDirection;
+
+            int frameHeight = texture.Height / frameCount;
+            int frame = charging ? (int)(chargeRadius * (frameCount - 2)) : frameCount - 1;
+
+            Rectangle sourceRectangle = new Rectangle(0, frame * frameHeight, texture.Width, frameHeight);
+
+            Vector2 origin = sourceRectangle.Size() / 2;
+            float rotation = projectile.velocity.ToRotation() + MathHelper.Pi - MathHelper.PiOver4;
+
+            spriteBatch.Draw(
+                texture,
+                position - Main.screenPosition,
+                sourceRectangle,
+                Color.White * projectile.Opacity,
+                rotation,
+                origin,
+                projectile.scale,
+                SpriteEffects.None,
+                0f);
+
+            return false;
+        }
+
+        public override bool CanDamage()
+        {
+            return false;
+        }
+    }
+}
