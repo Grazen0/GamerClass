7f92958bac3f08b53828a2d56ae4bc90574b2df2
ElCholoGamer <josedanielgrayson@gmail.com>
2021-10-03 09:41:54 -0500

    very important changes

---
+++ Items/Placeable/PeakEvolution.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GamerClass.Items.Placeable
+{
+    class PeakEvolution : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Lancer");
+            Tooltip.SetDefault("'Evolution at its finest'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 46;
+            item.height = 32;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.value = Item.buyPrice(0, 20, 0, 0);
+            item.createTile = ModContent.TileType<Tiles.PeakEvolution>();
+        }
+    }
+}
---
+++ Tiles/PeakEvolution.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace GamerClass.Tiles
+{
+    public class PeakEvolution : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.Origin = new Point16(2, 2);
+            TileObjectData.newTile.Width = 6;
+            TileObjectData.newTile.Height = 4;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16 };
+
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.addTile(Type);
+
+            disableSmartCursor = true;
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Painting");
+            AddMapEntry(new Color(99, 50, 30), name);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 48, 48, ModContent.ItemType<Items.Placeable.PeakEvolution>());
+        }
+    }
+}
