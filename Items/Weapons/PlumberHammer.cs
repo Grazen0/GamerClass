608fb089f62f2e8ef22ba43fdce21e070707c2a9
ElCholoGamer <josedanielgrayson@gmail.com>
2021-10-11 17:43:10 -0500

    add plumber's hammer

---
+++ Items/Weapons/PlumberHammer.cs
@@ -0,0 +1,31 @@
+﻿using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace GamerClass.Items.Weapons
+{
+    public class PlumberHammer : GamerWeapon
+    {
+        public override int RamUsage => 0;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Plumber's Hammer");
+            Tooltip.SetDefault("'It has some japanese scribbles on it'");
+        }
+
+        public override void SafeSetDefaults()
+        {
+            item.width = 24;
+            item.height = 48;
+            item.noMelee = true;
+            item.damage = 20;
+            item.useTime = item.useAnimation = 30;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.noUseGraphic = true;
+            item.UseSound = mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/PlumberHammer");
+            item.shoot = ModContent.ProjectileType<Projectiles.FlyingHammer>();
+            item.shootSpeed = 15;
+            item.autoReuse = true;
+        }
+    }
+}
---
+++ Projectiles/FlyingHammer.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace GamerClass.Projectiles
+{
+    public class FlyingHammer : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = projectile.height = 14;
+            projectile.hostile = false;
+            projectile.friendly = true;
+        }
+
+        public float RotationTimer
+        {
+            get => projectile.ai[0];
+            set => projectile.ai[0] = value;
+        }
+
+        public override void AI()
+        {
+            if (++RotationTimer > 4)
+            {
+                RotationTimer = 0;
+                projectile.rotation += MathHelper.PiOver2 * projectile.direction;
+            }
+
+            projectile.velocity.Y = MathHelper.Min(projectile.velocity.Y + 0.3f, 25f);
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D texture = Main.projectileTexture[projectile.type];
+
+            Vector2 origin = texture.Size() / 2;
+            Color color = GetAlpha(lightColor) ?? lightColor;
+
+            spriteBatch.Draw(
+                texture,
+                projectile.Center - Main.screenPosition,
+                null,
+                color * projectile.Opacity,
+                projectile.rotation,
+                origin,
+                projectile.scale,
+                projectile.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally,
+                0f);
+
+            return false;
+        }
+    }
+}
