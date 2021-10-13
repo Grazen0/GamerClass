c11d4a1a14a32e2d90ce37e49fa635c90d57e627
ElCholoGamer <josedanielgrayson@gmail.com>
2021-10-12 19:39:03 -0500

    add space invaders cannon

---
+++ Items/Weapons/SpaceInvadersCannon.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GamerClass.Items.Weapons
+{
+    public class SpaceInvadersCannon : GamerWeapon
+    {
+        public override int RamUsage => 0;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Dusty Joystick");
+            Tooltip.SetDefault("'There's some sort of martian scribble on it'");
+        }
+
+        public override void SafeSetDefaults()
+        {
+            item.noMelee = true;
+            item.damage = 22;
+            item.useTime = item.useAnimation = 40;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.UseSound = mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/SpaceInvadersCannon");
+            item.shoot = ModContent.ProjectileType<Projectiles.DustyLaserCannon.ShoddyBeam>();
+            item.shootSpeed = 30;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Vector2 front = Vector2.Normalize(new Vector2(speedX, speedY));
+            position += front * 12f;
+
+            return true;
+        }
+    }
+}
---
+++ Projectiles/DustyLaserCannon/ShoddyExplosion.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace GamerClass.Projectiles.DustyLaserCannon
+{
+    public class ShoddyExplosion : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[projectile.type] = 6;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = projectile.height = 42;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.light = 0.3f;
+            projectile.timeLeft = 30;
+        }
+
+        private void RunAnimation()
+        {
+            if (++projectile.frameCounter > 5)
+            {
+                projectile.frameCounter = 0;
+                if (++projectile.frame >= Main.projFrames[projectile.type])
+                {
+                    projectile.frame = 0;
+                }
+            }
+        }
+
+        public override bool CanDamage()
+        {
+            return false;
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            RunAnimation();
+
+            int pad = 6;
+            Texture2D texture = Main.projectileTexture[projectile.type];
+            int frameHeight = texture.Height / Main.projFrames[projectile.type] - pad;
+
+            Vector2 origin = new Vector2(texture.Width, frameHeight) / 2;
+            Rectangle sourceRectangle = new Rectangle(0, projectile.frame * (frameHeight + pad), frameHeight, texture.Width);
+            Color color = GetAlpha(lightColor) ?? lightColor;
+
+            spriteBatch.Draw(
+                texture,
+                projectile.Center - Main.screenPosition,
+                sourceRectangle,
+                color * projectile.Opacity,
+                projectile.rotation,
+                origin,
+                projectile.scale,
+                SpriteEffects.None,
+                0f);
+
+            return false;
+        }
+    }
+}
