5583b945f237e6e2d57fa365e0dc48ded8be8e43
ElCholoGamer <josedanielgrayson@gmail.com>
2021-10-29 18:22:40 -0500

    add pack whack

--- Items/Weapons/PackWhack.cs
+++ Items/Weapons/PackWhack.cs
@@ -4,7 +4,7 @@ using Terraria.ModLoader;
 
 namespace GamerClass.Items.Weapons
 {
-    public abstract class PackWhack : GamerWeapon
+    public class PackWhack : GamerWeapon
     {
         public override void SetStaticDefaults()
         {
@@ -13,15 +13,31 @@ namespace GamerClass.Items.Weapons
 
         public override void SafeSetDefaults()
         {
-            item.damage = 12;
-            item.value = Item.sellPrice(silver: 20);
+            item.damage = 26;
+            item.knockBack = 2f;
+            item.noMelee = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.noUseGraphic = true;
+            item.UseSound = SoundID.Item1;
+            item.useTime = item.useAnimation = 35;
+            item.shoot = ModContent.ProjectileType<Projectiles.FlyingBackpack>();
+            item.shootSpeed = 15f;
+            item.value = Item.sellPrice(silver: 5);
+            item.rare = ItemRarityID.Blue;
         }
 
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
 
-            recipe.AddIngredient(ItemID.Silk, 15);
+            recipe.AddIngredient(ItemID.Silk, 25);
+            recipe.AddIngredient(ItemID.WhiteString, 4);
+            recipe.AddRecipeGroup(RecipeGroupID.IronBar, 2);
+
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+
+            recipe.AddRecipe();
         }
     }
 }
