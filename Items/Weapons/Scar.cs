1ee0c814fd3700107ab53ffa8460915d9208a21a
ElCholoGamer <josedanielgrayson@gmail.com>
2021-10-29 18:21:51 -0500

    reduce scar sfx volume

--- Items/Weapons/Scar.cs
+++ Items/Weapons/Scar.cs
@@ -31,7 +31,7 @@ namespace GamerClass.Items.Weapons
             item.noMelee = true;
             item.damage = 40;
             item.useStyle = ItemUseStyleID.HoldingOut;
-            item.UseSound = mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/Scar");
+            item.UseSound = mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/Scar").WithVolume(0.5f);
             item.shootSpeed = 7;
             item.shoot = ModContent.ProjectileType<Projectiles.ScarBullet>();
             item.knockBack = 1;
@@ -55,7 +55,7 @@ namespace GamerClass.Items.Weapons
             {
                 // Shoot
                 item.useTime = item.useAnimation = 10;
-                item.UseSound = charge > 0 ? mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/Scar") : SoundID.Item98;
+                item.UseSound = charge > 0 ? mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/Scar").WithVolume(0.5f) : SoundID.Item98;
                 ramUsage = 2;
 
                 return base.CanUseItem(player);
