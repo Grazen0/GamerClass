204a3d2bd3196b72e7321795334822500a6e9b28
ElCholoGamer <josedanielgrayson@gmail.com>
2021-11-03 22:29:07 -0500

    add dev cheats accessory

---
+++ Items/Accessories/Misc/DevCheats.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GamerClass.Items.Accessories.Motherboards
+{
+    public class DevCheats : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Developer's Cheats");
+            Tooltip.SetDefault("'You probably shouldn't have this'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = item.height = 40;
+            item.rare = ItemRarityID.Purple;
+            item.accessory = true;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            var modPlayer = player.GetModPlayer<GamerPlayer>();
+
+            modPlayer.ramUsageMult = 0f;
+            modPlayer.maxRam2 += 1000;
+        }
+    }
+}
